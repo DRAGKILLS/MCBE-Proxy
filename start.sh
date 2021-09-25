f2f0dd040c090d29c178dddebf2fbf03e5e3ec63
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-09-25 05:48:36 -0700

    set start.sh good

--- start.sh
+++ start.sh
@@ -1,1 +1,15 @@
+if [ -d "bin/php7/bin/php" ]; then
+  echo ""
+else
+  echo "Install PHP Binary to start the proxy"
+  exit 1
+fi
+
+if [ -d "src/proxy/start.php" ]; then
+  echo ""
+else
+  echo "start.php not found"
+  exit 1
+fi
+
 bin/php7/bin/php src/proxy/start.php
