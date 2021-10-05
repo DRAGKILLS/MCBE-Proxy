9fd3eb74ef9963cb279d9d439c6c495965d5c534
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-10-05 11:08:13 +0300

    better start.cmd

--- start.cmd
+++ start.cmd
@@ -1,21 +1,20 @@
 @echo off
+
 title Proxy by Frago9876543210
-set BIN_PATH = "null"
-set PROXY_PATH = "null"
+
 :proxy_start
-if exist bin\php\php.exe (
-    set BIN_PATH = bin\php\php.exe
-) else (
+if not exist bin\php\php.exe (
     echo Install PHP Binary to start the proxy
     exit 1
 )
-if exist src\proxy\start.php (
-    set PROXY_PATH = src\proxy\start.php
-) else (
+
+if not exist src\proxy\start.php (
     echo start.php not found
     exit 1
 )
+
 bin\php\php.exe src\proxy\start.php
 timeout 5
 goto proxy_start
+
 pause
